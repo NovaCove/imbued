244733384c3f5270c8de242f16fdfe29bac68548
Trey Tacon <ttacon@gmail.com>
2025-05-28 13:11:04 -0400

    fix: cleanup how we pass directories in

--- scripts/macos/install.sh
+++ scripts/macos/install.sh
@@ -4,9 +4,30 @@
 
 set -e
 
-# Get the directory of this script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
+# Get the bin and script dir from the first and second arguments to this script.
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <bin_dir> <script_dir>"
+    exit 1
+fi
+BIN_DIR="$1"
+SCRIPT_DIR="$2"
+PROJECT_DIR="$(dirname "$SCRIPT_DIR")"
+
+# Check if the bin directory exists
+if [ ! -d "$BIN_DIR" ]; then
+    echo "Error: Bin directory '$BIN_DIR' does not exist."
+    exit 1
+fi
+# Check if the script directory exists
+if [ ! -d "$SCRIPT_DIR" ]; then
+    echo "Error: Script directory '$SCRIPT_DIR' does not exist."
+    exit 1
+fi
+# Check if the imbued binary exists
+if [ ! -f "$BIN_DIR/imbued" ]; then
+    echo "Error: imbued binary '$BIN_DIR/imbued' does not exist."
+    exit 1
+fi
 
 # Create the imbued directory structure
 echo "Creating imbued directory structure..."
@@ -16,8 +37,8 @@ mkdir -p ~/.imbued/logs
 echo "Installing launchd plist file..."
 # Replace ~ with the actual home directory in the plist file
 # sed "s|~|$HOME|g" "$SCRIPT_DIR/com.novacove.imbued.plist" > /tmp/com.novacove.imbued.plist
-cp /tmp/com.novacove.imbued.plist ~/Library/LaunchAgents/com.novacove.imbued.plist
-rm /tmp/com.novacove.imbued.plist
+cp "$SCRIPT_DIR/com.novacove.imbued.plist" ~/Library/LaunchAgents/com.novacove.imbued.plist
+# rm /tmp/com.novacove.imbued.plist
 
 # Load the launchd service
 echo "Loading launchd service..."
